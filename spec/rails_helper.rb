f817a00ec31917c68672d75127a354c2901bea2b
Mike McKee <mikecm@gmail.com>
2018-08-29 14:25:30 -0600

    Add database cleaner and simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'database_cleaner'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -59,3 +64,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+DatabaseCleaner.strategy = :truncation
