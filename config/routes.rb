e42297c70dd2cd0289d93b62e3a8df8a89202df9
Mike McKee <mikecm@gmail.com>
2018-08-29 21:23:27 -0600

    Add admin namespace, create admin/show controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :shows, only: %i[new]
+  end
 end
--- spec/features/admin/shows/add_new_show_spec.rb
+++ spec/features/admin/shows/add_new_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'As an admin' do
   describe 'when I visit /admin/shows/new' do
     scenario 'I fill out the show form and see the show page' do
-      visit '/admin/shows/new'
+      visit new_admin_show_path
 
       fill_in :show_title, with: 'Futurama'
       click_on 'Create Show'
