693b786b3fde14baa5533d312c387ff8d2c9e390
Mike McKee <mikecm@gmail.com>
2018-08-29 21:18:30 -0600

    Add feature spec for admin adding a new show

---
+++ spec/features/admin/shows/add_new_show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'As an admin' do
+  describe 'when I visit /admin/shows/new' do
+    scenario 'I fill out the show form and see the show page' do
+      visit '/admin/shows/new'
+
+      fill_in :show_title, with: 'Futurama'
+      click_on 'Create Show'
+
+      expect(current_path).to eq("/admin/shows/#{Show.last.id}")
+      expect(page).to have_content('Futurama')
+    end
+  end
+end
