3089a3e6a3cd1e863340c2d03794ae875c335219
Mike McKee <mikecm@gmail.com>
2018-09-01 20:04:33 -0600

    Add feature spec for /shows page'

---
+++ spec/features/shows/visitor_and_user_can_see_all_shows_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'As a visitor or user' do
+  describe 'when I visit /shows' do
+    before(:each) do
+      @show1, @show2 = create_list(:shows, 2)
+    end
+    scenario 'I see a list of all TV shows' do
+      visit shows_path
+
+      expect(page).to have_content(@show1.title)
+      within("#show-year-id-#{@show1.id}") do
+        expect(page).to have_content(@show1.year)
+      end
+      expect(page).to have_content(@show2.title)
+      within("#show-year-id-#{@show2.id}") do
+        expect(page).to have_content(@show2.year)
+      end
+    end
+    scenario 'I can click on a show title to get to its show page' do
+      visit shows_path
+
+      click_link @show1.title
+
+      expect(current_path).to eq(show_path(@show1))
+      expect(page).to have_content(@show1.title)
+    end
+  end
+end
