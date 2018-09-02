4f399f1ee5fbe3d420140dda4ac39e4bf1a8f230
Mike McKee <mikecm@gmail.com>
2018-09-02 10:23:21 -0600

    styling changes for public show show page

--- spec/factories/shows.rb
+++ spec/factories/shows.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :show do
     sequence(:title) { |n| "Show Title #{n}" }
     sequence(:year)  { rand(1950..2018)      }
+    image            { 'placeholder.png'     }
   end
 end
