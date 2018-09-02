8979a1ef3b221f9c9baa8c35619f184ce35d0bc2
Mike McKee <mikecm@gmail.com>
2018-09-02 10:46:01 -0600

    adding default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(
+  name: 'Mike',
+  email: 'mikecm@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: true
+)
